77604bdfe5c3f4983fa6954bd5161cf424731c81
ayush893 <mauryaayush893@gmail.com>
2021-05-22 09:26:22 +0530

    refactor show

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -19,11 +19,13 @@ class UrlsController < ApplicationController
   end
 
   def show
-    @url = Url.find_by(id: $url_id)
+  	byebug
+  	short_url = params[:id]
+    @url = Url.find_by(short_url: "#{"http://localhost:3000/urls"}/#{short_url}")
     redirect_to @url.sanitize
+	end
   end
   private
   def url_params
     params.require(:url).permit(:long_url)
   end
-end
